591ef08e6716fe166fe74b68695cc33ed9fc4019
Crowdbotics <team@crowdbotics.com>
2021-08-23 22:10:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mscreative_30057Identifier",
+  url: "https://mscreative_30057Identifier.botics.co",
+  api: "https://mscreative_30057Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
